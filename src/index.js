c72b07ee87eb0342627e7a495ae5f654d0525654
Anup Mahato <anupmahato033@gmail.com>
2022-08-05 21:24:36 +0530

    added ProcFile for heroku

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 
 const express = require("express")
 const app = express();
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log("server running on port 3000")
+app.listen(PORT, () => {
+    console.log("server running on port : ", PORT)
 })
 
 app.get("/", (req, res, next) => {
